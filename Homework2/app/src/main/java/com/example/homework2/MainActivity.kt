2375446ee2811f9cec926e1c76920e967f362a96
Dallas LaComb <dallaslacombdrive@gmail.com>
2024-02-09 13:52:39 -0500

    Changes pizza image based off of radio selection

--- Homework2/app/src/main/java/com/example/homework2/MainActivity.kt
+++ Homework2/app/src/main/java/com/example/homework2/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.homework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun setPizzaImageView(view:View){
+//      Changes the image of Pizza based off of radio button selection.
+        val imageIdOfSelectedPizza = when (view.id){
+            R.id.rb_pepperoni -> R.drawable.pepperoni
+            R.id.rb_bbq_chicken -> R.drawable.bbq_chicken
+            R.id.rb_margherita -> R.drawable.margheritta
+            R.id.rb_hawaiian -> R.drawable.hawaiian
+            else -> R.drawable.pizza_crust
+        }
+        findViewById<ImageView>(R.id.iv_default_pizza).setImageResource(imageIdOfSelectedPizza)
+    }
+
 }
