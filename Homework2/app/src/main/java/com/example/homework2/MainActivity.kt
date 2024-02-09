1b3454fb0da9b12223869de22c9b6a4b903babf1
Dallas LaComb <dallaslacombdrive@gmail.com>
2024-02-09 18:33:52 -0500

    increment counter working quantity

--- Homework2/app/src/main/java/com/example/homework2/MainActivity.kt
+++ Homework2/app/src/main/java/com/example/homework2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.Spinner
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
 
         val pizzaSizeList = listOf("Medium ($9.99)", "Large($13.99)", "Extra Large ($15.99)", "Party Size ($25.99)")
@@ -39,8 +41,34 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             }
         })
 
+        updateQuantity()
 
         }
+
+    private fun updateQuantity() {
+        val tvQuantity = findViewById<TextView>(R.id.tv_quantity_number)
+        val buIncrementButton = findViewById<Button>(R.id.bu_increment_quantity)
+        val buDecrementButton = findViewById<Button>(R.id.bu_decrement_quantity)
+
+        // Set an OnClickListener for the Increment button
+        buIncrementButton.setOnClickListener {
+            // Parse the current quantity to an integer, increment it, and set back
+            var quantity = tvQuantity.text.toString().toInt()
+            quantity++
+            tvQuantity.text = quantity.toString()
+        }
+
+        // Set an OnClickListener for the Decrement button
+        buDecrementButton.setOnClickListener {
+            // Parse the current quantity to an integer, decrement it, and ensure it does not go below a certain value (e.g., 0)
+            var quantity = tvQuantity.text.toString().toInt()
+            if (quantity > 0) { // Prevents quantity from going below 0
+                quantity--
+                tvQuantity.text = quantity.toString()
+            }
+        }
+    }
+
     override fun onNothingSelected(parent: AdapterView<*>?) {
 //        Toast.makeText(this, "Nothing is selected!", Toast.LENGTH_SHORT).show()
     }
@@ -70,9 +98,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             switchText = "No, $0.00"
             findViewById<SeekBar>(R.id.sb_spiciness_level).visibility = View.INVISIBLE;
             findViewById<TextView>(R.id.tv_spiciness_level).visibility = View.INVISIBLE;
-
-
         }
         switchSpicy.text=switchText
     }
+
 }
